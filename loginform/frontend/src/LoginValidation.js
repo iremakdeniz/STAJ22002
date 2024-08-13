68e4dd142bb46a25e7a2e55dd304f42af635945e
İrem Akdeniz <154349335+iremakdeniz@users.noreply.github.com>
2024-08-13 11:13:25 +0300

    Projeler eklenmiştir
    Projeler eklenmiştir

---
+++ loginform/frontend/src/LoginValidation.js
@@ -0,0 +1,25 @@
+function Validation(values) {
+    let error = {};
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+
+    if (values.email === "") {
+        error.email = "Email boş bırakılamaz!";
+    } else if (!email_pattern.test(values.email)) {
+        error.email = "Email eşleşmiyor!";
+    } else {
+        error.email = "";
+    }
+
+    if (values.password === "") {
+        error.password = "Şifre boş bırakılamaz!";
+    } else if (!password_pattern.test(values.password)) {
+        error.password = "Şifre eşleşmiyor! ";
+    } else {
+        error.password = "";
+    }
+
+    return error;
+}
+
+export default Validation;
